f40e5977e4d2f20d2a9ba5fdc7a5873875152e56
Vivek Naskar <viveknaskar2912@gmail.com>
2026-03-18 02:12:28 +0530

    fixed the library mismatch issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/kapde/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/kapde/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
